f64d85135afda6b36750e2961fc57a6988fa1ce9
Miguel Ángel Martín <miguelbemartin@gmail.com>
2019-08-13 16:36:01 +0200

    Add exceptions to client class

--- lib/Http/Client.php
+++ lib/Http/Client.php
@@ -4,6 +4,10 @@ namespace BufferSDK\Http;
 
 use BufferSDK\Auth\AuthorizationTokenInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -12,7 +16,7 @@ class Client implements ClientInterface
     /** @var string */
     protected $baseURL = 'https://api.bufferapp.com/1/';
 
-    /** @var HttpClientInterface  */
+    /** @var HttpClientInterface */
     private $httpClient;
 
     /**
@@ -33,15 +37,18 @@ class Client implements ClientInterface
      * @param array $options
      *
      * @return array
+     *
+     * @throws TransportExceptionInterface
+     * @throws DecodingExceptionInterface    When the body cannot be decoded to an array
+     * @throws TransportExceptionInterface   When a network error occurs
+     * @throws RedirectionExceptionInterface On a 3xx when $throw is true and the "max_redirects" option has been reached
+     * @throws ClientExceptionInterface      On a 4xx when $throw is true
+     * @throws ServerExceptionInterface      On a 5xx when $throw is true
      */
     public function createHttpRequest(string $method, string $endpoint, array $options = []): array
     {
-        try {
-            $response = $this->httpClient->request($method, $this->baseURL.$endpoint, $options);
-            $responseArray = $response->toArray();
-        } catch (TransportExceptionInterface $e) {
-            //TODO: Handle errors
-        }
+        $response = $this->httpClient->request($method, $this->baseURL.$endpoint, $options);
+        $responseArray = $response->toArray();
 
         return $responseArray;
     }
